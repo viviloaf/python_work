eef8cccba40c271bee9bb146ec04aa6fb7d027d2
Vinh Xuan Nguyen <vnguye34@students.kennesaw.edu>
2017-09-09 02:26:40 -0400

    exercise 9

---
+++ exercise9.py
@@ -0,0 +1,16 @@
+#Printing with python
+#Here's some new strage stuff, remember type it exactly.
+
+days = "Mon tues Wed Thurs Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# n\ means string represented on new line without having to type it out
+#ex, new line character
+print "Here are the days: ", days
+print "here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes,
+We'll be able to type a smuch as we like
+Even 4 lines if we want, or 5, or 6.
+"""
